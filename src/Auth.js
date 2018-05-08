3ec4eb5e55ea344852398d0325b89002370f6843
lzcrwxl <lzcrwxl@163.com>
2018-05-08 23:27:14 +0800

    update route

---
+++ src/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Auth extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return <div>Auth</div>
+  }
+}
+
+export default Auth;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,40 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
-import { Provider} from 'react-redux'
-import { BrowserRouter, Route, Link} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {BrowserRouter, Route, Link, Redirect, Switch} from 'react-router-dom'
+import Auth from './Auth'
+import Dashboard from './Dashboard'
 
-import App from './App'
-import { counter} from './index.redux'
-import { SSL_OP_PKCS1_CHECK_2 } from 'constants';
+import {counter} from './index.redux'
 
 // const reduxDevtools=window.devToolsExtension
-const store = createStore(counter,compose(
-  applyMiddleware(thunk),
-  window.devToolsExtension?window.devToolsExtension():f=>f
-))
+const store = createStore(counter, compose(applyMiddleware(thunk), window.devToolsExtension
+  ? window.devToolsExtension()
+  : f => f))
 
-function Erying() {
-  return <h2>二营</h2>
+
+class Test extends React.Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div>测试</div>
+    )
+  }
 }
-function Qibinglian() {
-  return <h2>骑兵连</h2>
-}
-ReactDom.render(
-  (<Provider  store={store}>
+
+ReactDom.render((
+  <Provider store={store}>
     <BrowserRouter>
       <div>
-        <ul>
-          <li>
-            <Link to='/'>一</Link>
-          </li>
-          <li>
-            <Link to='/erying'>二</Link>
-          </li>
-          <li>
-            <Link to='qibinglian'>骑兵连</Link>
-          </li>
-        </ul>
-        <Route path='/' exact component={App}></Route>
-        <Route path='/erying' component={Erying}></Route>
-        <Route path='/qibinglian' component={Qibinglian}></Route>
+        <Switch>
+          <Route path='/login' exact component={Auth}></Route>
+          <Route path='/dashboard' component={Dashboard}></Route>
+          <Redirect to='/dashboard'></Redirect>
+        </Switch>
+      
       </div>
     </BrowserRouter>
-  </Provider>),
-  document.getElementById('root')
-)
+  </Provider>
+), document.getElementById('root'))
