79675ba6343eab71b72a87cceb708535695b7159
lzcrwxl <lzcrwxl@163.com>
2018-05-31 23:00:46 +0800

    7-1

---
+++ src/component/avatar-selector/avatar-selector.js
@@ -0,0 +1,38 @@
+
+import React from 'react'
+import {Grid,List} from 'antd-mobile'
+class AvatarSelector extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={}
+  }
+  render(){
+    const avatarList='boy,woman,man,bull,chick,crab,girl,hedgehog,hippopotamus,koala,lemur,pig,tiger,whale,zebra'.split(',').map(v=>({
+      icon:require(`../img/${v}.png`),
+      text: v
+    }))
+    const gridHeader=this.state.text
+                    ?(<div>
+                      <span>已选择头像</span>
+                      <img src={this.state.icon} alt=""
+                      style={{width:20}}/>
+                    </div>)
+                    : <div>请选择头像</div>
+    return(
+      <div>
+        <List renderHeader={()=>gridHeader}>
+          <Grid 
+          data={avatarList} 
+          activeStyle={false} 
+          columnNum={5}
+          onClick={elm=>{
+            this.setState(elm)
+            this.props.selectorAvatar(elm.text)
+          }}></Grid>
+        </List>
+      </div>
+    )
+  }
+}
+
+export default AvatarSelector;
--- src/container/bossinfo/bossinfo.js
+++ src/container/bossinfo/bossinfo.js
@@ -1,10 +1,51 @@
 
 import React from 'react'
+import {NavBar,InputItem,TextareaItem<Button} from 'antd-mobile' 
+import AvatarSelector from '../../component/avatar-selector/avatar-selector'
+import { Button } from 'antd-mobile';
 
 class BossInfo extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      title:''
+    }
+  }
+  onChange(key,val){
+    this.setState({
+      [key]:val
+    })
+  }
   render(){
     return(
-      <div>完善信息</div>
+      <div>
+        <NavBar mode="dark">Boss完善信息页面</NavBar>
+        <AvatarSelector 
+          selectorAvatar={(imgname)=>{
+            this.setState({
+              avatar:imgname
+            })
+          }}
+        ></AvatarSelector>
+        <InputItem onChange={(v)=>this.onChange('title',v)}>
+          招聘职位
+        </InputItem>
+        <InputItem onChange={(v)=>this.onChange('company',v)}>
+          公司名称
+        </InputItem>
+        <InputItem onChange={(v)=>this.onChange('money',v)}>
+          职位薪资
+        </InputItem>
+        <TextareaItem 
+        onChange={(v)=>this.onChange('desc',v)}
+        rows={3}
+        autoHeight
+        title='职位要求'>
+          职位要求
+        </TextareaItem>
+        <Button type="primary">保存</Button>
+      </div>
+      
     )
   }
 }
